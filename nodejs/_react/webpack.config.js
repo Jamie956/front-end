178a7f10c63ebac10287af885ed7a643f44c3fb4
jamie <jamie@justek.us>
2018-03-02 10:12:50 +0800

    update react simple example

--- nodejs/_react/webpack.config.js
+++ nodejs/_react/webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path');
 
 module.exports = {
-  entry: './main.js',
+  entry: './src/js/main.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve(__dirname, './build'),
   },
   module: {
     rules: [{
