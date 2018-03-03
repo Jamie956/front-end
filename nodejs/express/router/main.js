fa34ff2c5aab43aaa6a72b7b6f3b0f509c71fd16
jamei <752492509@qq.com>
2018-03-03 12:50:11 +0800

    update router

--- nodejs/express/router/main.js
+++ nodejs/express/router/main.js
@@ -9,4 +9,20 @@ app.put('/user', (req, res) => res.send('Got a PUT request at /user'));
 
 app.delete('/user', (req, res) => res.send('Got a DELETE request at /user'));
 
+//localhost:3000/secret
+app.all('/secret', function (req, res, next) {
+  console.log('Accessing the secret section ...')
+  next()
+})
+
+//localhost:3000/users/34/books/8989
+app.get('/users/:userId/books/:bookId', function (req, res) {
+  res.send(req.params)
+})
+
+//localhost:3000/flights/LAX-SFO
+app.get('/flights/:from-:to', function (req, res) {
+  res.send(req.params)
+})
+
 app.listen(3000, () => console.log('listen on port: 3000'));
