14e81ae10868865d4fa989dd147b9dc4b1c52a47
jamie <jamie@justek.us>
2018-03-05 11:48:15 +0800

    update

--- nodejs/webpack/html-plugin/webpack.config.js
+++ nodejs/webpack/html-plugin/webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
-// const webpack = require('webpack'); //to access built-in plugins
 
 module.exports = {
 	entry: './src/main.js',
