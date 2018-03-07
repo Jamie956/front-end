107c33660d6993ef89fd182affaba09c8930515b
jamie <jamie@justek.us>
2018-03-07 15:38:42 +0800

    init nodejs path

---
+++ nodejs/path/main.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+console.log('basename => '+path.basename('/foo/bar/baz/asdf/quux.html', '.html'));
+
+console.log('dirname => '+path.dirname('/foo/bar/baz/asdf/quux'));
+
+console.log('extname => '+path.extname('index.html'));
+
+console.log('__dirname => '+__dirname);
+
+console.log('join => '+path.join(__dirname, 'css'));
+
+console.log('join => '+path.join('/foo', 'bar', 'baz/asdf', 'quux', '..'));
+
+console.log('parse => '+path.parse('C:\\path\\dir\\file.txt'));
+
+console.log('resolve => '+path.resolve());
+
+console.log('resolve => '+path.resolve('css'));
+
+console.log('resolve => '+path.resolve('/foo/bar', './baz'));
+
+const css = require('../../build/bundle.server');
