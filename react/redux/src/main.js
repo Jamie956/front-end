57bae50f7cfd1867f38f7116832f60f83c046329
jamei <752492509@qq.com>
2018-03-08 22:57:53 +0800

    update redux

--- react/redux/src/main.js
+++ react/redux/src/main.js
@@ -13,7 +13,7 @@ const reduer = function(state, action){
 const store = createStore(reduer, 0);
 
 store.subscribe(() => {
-    console.log("store changed", store.getState());
+  console.log("store changed", store.getState());
 })
 
 store.dispatch({type: "INC", payload: 1});
