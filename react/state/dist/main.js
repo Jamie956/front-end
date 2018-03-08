16ad2d7970a27de4028cb8227209a3bef027b162
jamie <jamie@justek.us>
2018-03-08 12:06:10 +0800

    update state

--- react/state/dist/main.js
+++ react/state/dist/main.js
@@ -19776,10 +19776,6 @@
 
 	var _react2 = _interopRequireDefault(_react);
 
-	var _Header = __webpack_require__(160);
-
-	var _Header2 = _interopRequireDefault(_Header);
-
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -19794,35 +19790,19 @@
 		function Layout() {
 			_classCallCheck(this, Layout);
 
-			return _possibleConstructorReturn(this, (Layout.__proto__ || Object.getPrototypeOf(Layout)).apply(this, arguments));
+			var _this = _possibleConstructorReturn(this, (Layout.__proto__ || Object.getPrototypeOf(Layout)).call(this));
+
+			_this.state = { name: "Alo" };
+			return _this;
 		}
 
 		_createClass(Layout, [{
 			key: "render",
-
-			//state ==================================
-			// constructor(){
-			// 	super();
-			// 	this.state = {name: "Alo"};
-			// }
-			// render(){
-			// 	return (
-			// 		<div>
-			// 			{this.state.name}
-			// 		</div>
-			// 	);
-			// }
-
-			//props ==================================
 			value: function render() {
-				var guest = 'Hello World.';
-				var again = 'Hello World Again.';
-
 				return _react2.default.createElement(
 					"div",
 					null,
-					_react2.default.createElement(_Header2.default, { foo: guest }),
-					_react2.default.createElement(_Header2.default, { foo: again })
+					this.state.name
 				);
 			}
 		}]);
@@ -19831,55 +19811,6 @@
 	}(_react2.default.Component);
 
 	exports.default = Layout;
-
-/***/ }),
-/* 160 */
-/***/ (function(module, exports, __webpack_require__) {
-
-	"use strict";
-
-	Object.defineProperty(exports, "__esModule", {
-		value: true
-	});
-
-	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-	var _react = __webpack_require__(1);
-
-	var _react2 = _interopRequireDefault(_react);
-
-	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-
-	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-	var Header = function (_React$Component) {
-		_inherits(Header, _React$Component);
-
-		function Header() {
-			_classCallCheck(this, Header);
-
-			return _possibleConstructorReturn(this, (Header.__proto__ || Object.getPrototypeOf(Header)).apply(this, arguments));
-		}
-
-		_createClass(Header, [{
-			key: "render",
-			value: function render() {
-				return _react2.default.createElement(
-					"div",
-					null,
-					this.props.foo
-				);
-			}
-		}]);
-
-		return Header;
-	}(_react2.default.Component);
-
-	exports.default = Header;
 
 /***/ })
 /******/ ]);
--- react/state/src/Layout.js
+++ react/state/src/Layout.js
@@ -1,31 +1,15 @@
 import React from "react";
-import Header from "./Header";
 
 export default class Layout extends React.Component {
-	//state ==================================
-	// constructor(){
-	// 	super();
-	// 	this.state = {name: "Alo"};
-	// }
-	// render(){
-	// 	return (
-	// 		<div>
-	// 			{this.state.name}
-	// 		</div>
-	// 	);
-	// }
-
-	//props ==================================
+	constructor(){
+		super();
+		this.state = {name: "Alo"};
+	};
 	render(){
-		const guest = 'Hello World.';
-		const again = 'Hello World Again.';
-		
 		return (
 			<div>
-				<Header foo={guest}/>
-				<Header foo={again}/>
+				{this.state.name}
 			</div>
 		);
 	}
-
 }
