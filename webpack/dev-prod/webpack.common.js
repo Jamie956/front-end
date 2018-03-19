57e55defb47650d4edd93ac55319781e27030ee1
jamie <752492509@qq.com>
2018-03-19 12:35:08 +0800

    update

--- webpack/dev-prod/webpack.common.js
+++ webpack/dev-prod/webpack.common.js
@@ -2,14 +2,14 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+	entry: {
+		app: './src/main.js'
+	},
 	plugins: [
 		new HtmlWebpackPlugin({
 			template: './src/index.html'
 		})
 	],
-	entry: {
-		app: './src/main.js'
-	},
 	output: {
 		filename: '[name].bundle.js',
 		path: path.resolve(__dirname, 'dist')
