25d8849bc33f01f516e56c47c2199df171530123
jamie <752492509@qq.com>
2018-03-19 14:38:20 +0800

    update

--- webpack/html-plugin/webpack.config.js
+++ webpack/html-plugin/webpack.config.js
@@ -1,13 +1,15 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	entry: './src/main.js',
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./src/index.html"
+		})
+	],
 	output: {
 		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'dist'),
-	},
-	plugins: [
-    new HtmlWebpackPlugin()
-  ]
+		path: path.resolve(__dirname, 'dist')
+	}
 };
