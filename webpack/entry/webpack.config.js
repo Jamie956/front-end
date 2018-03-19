c2625d374c90caef7544be729ab74924f6caf7d0
jamie <752492509@qq.com>
2018-03-19 15:12:08 +0800

    update

--- webpack/entry/webpack.config.js
+++ webpack/entry/webpack.config.js
@@ -2,11 +2,11 @@ const path = require('path');
 
 module.exports = {
 	entry: {
-    app: './src/app.js',
-    vendors: './src/vendors.js'
+		app: './src/app.js',
+		vendors: './src/vendors.js'
 	},
 	output: {
 		filename: '[name].bundle.js',
-		path: path.resolve(__dirname, 'dist'),
+		path: path.resolve(__dirname, 'dist')
 	}
 };
