866a74e7b2294f6ea660074c89a7f988d259fa2f
jamie <752492509@qq.com>
2018-03-19 14:30:34 +0800

    update

--- webpack/sourcemap/webpack.config.js
+++ webpack/sourcemap/webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	entry: './src/main.js',
+	plugins: [new HtmlWebpackPlugin()],
+	devtool: 'inline-source-map',
 	output: {
 		filename: 'bundle.js',
 		path: path.resolve(__dirname, 'dist'),
-	},
-	devtool: 'inline-source-map'
+	}
 };
