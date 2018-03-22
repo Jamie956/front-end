2863650febc72097ebf5e8a34694cdc7b73ae98f
jamie <752492509@qq.com>
2018-03-23 00:41:28 +0800

    update

--- react/props/README.md
+++ react/props/README.md
@@ -1,5 +1,2 @@
 npm i
-npm run dev
-npm run build
-
-http://localhost:8080
+npm start
--- react/props/src/Layout.js
+++ react/props/src/Layout.js
@@ -1,5 +1,14 @@
 import React from "react";
-import Header from "./Header";
+
+class Header extends React.Component {
+	render(){
+		return (
+			<div>
+				{this.props.foo}
+			</div>
+		);
+	}
+}
 
 export default class Layout extends React.Component {
 	render(){
