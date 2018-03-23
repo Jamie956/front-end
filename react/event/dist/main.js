9118d69cafb9281488d9c57f5b2cab347cda1c65
jamie <752492509@qq.com>
2018-03-23 10:12:13 +0800

    update

--- react/event/dist/main.js
+++ react/event/dist/main.js
@@ -19776,10 +19776,6 @@
 
 	var _react2 = _interopRequireDefault(_react);
 
-	var _Header = __webpack_require__(160);
-
-	var _Header2 = _interopRequireDefault(_Header);
-
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -19788,18 +19784,50 @@
 
 	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-	var Layout = function (_React$Component) {
-		_inherits(Layout, _React$Component);
+	var Header = function (_React$Component) {
+		_inherits(Header, _React$Component);
+
+		function Header() {
+			_classCallCheck(this, Header);
+
+			return _possibleConstructorReturn(this, (Header.__proto__ || Object.getPrototypeOf(Header)).apply(this, arguments));
+		}
+
+		_createClass(Header, [{
+			key: "handleChange",
+			value: function handleChange(e) {
+				var title = e.target.value;
+				this.props.changeTitle(title);
+			}
+		}, {
+			key: "render",
+			value: function render() {
+				return _react2.default.createElement(
+					"div",
+					null,
+					_react2.default.createElement(
+						"h1",
+						null,
+						this.props.title
+					),
+					_react2.default.createElement("input", { value: this.props.title, onChange: this.handleChange.bind(this) })
+				);
+			}
+		}]);
+
+		return Header;
+	}(_react2.default.Component);
+
+	var Layout = function (_React$Component2) {
+		_inherits(Layout, _React$Component2);
 
 		function Layout() {
 			_classCallCheck(this, Layout);
 
-			var _this = _possibleConstructorReturn(this, (Layout.__proto__ || Object.getPrototypeOf(Layout)).call(this));
+			var _this2 = _possibleConstructorReturn(this, (Layout.__proto__ || Object.getPrototypeOf(Layout)).call(this));
 
-			_this.state = {
-				title: "Here We Go!"
-			};
-			return _this;
+			_this2.state = { title: "Here We Go!" };
+			return _this2;
 		}
 
 		_createClass(Layout, [{
@@ -19813,7 +19841,7 @@
 				return _react2.default.createElement(
 					"div",
 					null,
-					_react2.default.createElement(_Header2.default, { changeTitle: this.changeTitle.bind(this), title: this.state.title })
+					_react2.default.createElement(Header, { changeTitle: this.changeTitle.bind(this), title: this.state.title })
 				);
 			}
 		}]);
@@ -19822,66 +19850,6 @@
 	}(_react2.default.Component);
 
 	exports.default = Layout;
-
-/***/ }),
-/* 160 */
-/***/ (function(module, exports, __webpack_require__) {
-
-	"use strict";
-
-	Object.defineProperty(exports, "__esModule", {
-	  value: true
-	});
-
-	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-	var _react = __webpack_require__(1);
-
-	var _react2 = _interopRequireDefault(_react);
-
-	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-
-	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-	var Header = function (_React$Component) {
-	  _inherits(Header, _React$Component);
-
-	  function Header() {
-	    _classCallCheck(this, Header);
-
-	    return _possibleConstructorReturn(this, (Header.__proto__ || Object.getPrototypeOf(Header)).apply(this, arguments));
-	  }
-
-	  _createClass(Header, [{
-	    key: "handleChange",
-	    value: function handleChange(e) {
-	      var title = e.target.value;
-	      this.props.changeTitle(title);
-	    }
-	  }, {
-	    key: "render",
-	    value: function render() {
-	      return _react2.default.createElement(
-	        "div",
-	        null,
-	        _react2.default.createElement(
-	          "h1",
-	          null,
-	          this.props.title
-	        ),
-	        _react2.default.createElement("input", { value: this.props.title, onChange: this.handleChange.bind(this) })
-	      );
-	    }
-	  }]);
-
-	  return Header;
-	}(_react2.default.Component);
-
-	exports.default = Header;
 
 /***/ })
 /******/ ]);
--- react/event/src/Layout.js
+++ react/event/src/Layout.js
@@ -1,18 +1,28 @@
 import React from "react";
-import Header from "./Header";
+
+class Header extends React.Component{
+	handleChange(e){
+		const title = e.target.value;
+		this.props.changeTitle(title);
+	}
+	render(){
+		return (
+			<div>
+				<h1>{this.props.title}</h1>
+				<input value={this.props.title} onChange={this.handleChange.bind(this)} />
+			</div>
+		);
+	}
+}
 
 export default class Layout extends React.Component {
   constructor(){
 		super();
-		this.state = {
-			title: "Here We Go!"
-		}
+		this.state = {title: "Here We Go!"}
 	}
-
 	changeTitle(title){
 		this.setState({title});
   }
-  
 	render() {
 		return (
       <div>
