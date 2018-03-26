086724f2ee6b05ce7dcbbc54ab6f005a3e7fe9a5
jamie <752492509@qq.com>
2018-03-26 17:49:31 +0800

    update chunkhash

--- webpack/04-chunkhash/webpack.config.js
+++ webpack/04-chunkhash/webpack.config.js
@@ -1,9 +1,12 @@
 const path = require('path');
 
 module.exports = {
-	entry: './src/main.js',
+	entry: {
+		app: './src/app',
+		vendors: './src/vendors'
+	},
 	output: {
-		filename: '[name].[chunkhash].js',
+		filename: '[name]-[chunkhash].js',
 		path: path.resolve(__dirname, 'dist')
 	}
 };
