46d4e5e5b7ad80e2d318a42bdeb43dfafc3cb3f6
jamie <752492509@qq.com>
2018-03-26 11:04:18 +0800

    update

--- react/template/webpack.config.js
+++ react/template/webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: "./src/main.js",
+  entry: "./src/main.jsx",
   module: {
     loaders: [{
       test: /\.jsx?$/,
