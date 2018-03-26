e5d2b9fcf0b12b4ebddb836393d5a917da38db43
jamie <752492509@qq.com>
2018-03-26 17:32:48 +0800

    rename

--- webpack/02-entry/webpack.config.js
+++ webpack/02-entry/webpack.config.js
@@ -1,10 +1,13 @@
 const path = require('path');
 
 module.exports = {
+	//======================== 1
 	entry: {
-		app: './src/app.js',
-		vendors: './src/vendors.js'
+		app: './src/app',
+		vendors: './src/vendors'
 	},
+	//======================== 2
+	// entry:['./src/app','./src/vendors'],
 	output: {
 		filename: '[name].bundle.js',
 		path: path.resolve(__dirname, 'dist')
