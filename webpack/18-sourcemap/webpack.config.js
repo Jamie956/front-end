5007179f892d1fe2acc6c4df677ed3294b9a2e94
jamie <752492509@qq.com>
2018-03-27 18:07:29 +0800

    update source map

--- webpack/18-sourcemap/webpack.config.js
+++ webpack/18-sourcemap/webpack.config.js
@@ -1,11 +1,7 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	entry: './src/main.js',
-	plugins: [
-		new HtmlWebpackPlugin()
-	],
 	devtool: 'inline-source-map',
 	output: {
 		filename: 'bundle.js',
