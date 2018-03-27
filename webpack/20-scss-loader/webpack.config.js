de0c8cc6bb2e6781781f7d412889229409380355
jamie <752492509@qq.com>
2018-03-27 18:15:41 +0800

    update scss

--- webpack/20-scss-loader/webpack.config.js
+++ webpack/20-scss-loader/webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	entry: './src/main.js',
@@ -15,11 +14,6 @@ module.exports = {
 			}]
 		}]
 	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: "./src/index.html"
-		})
-	],
 	output: {
 		filename: 'bundle.js',
 		path: path.resolve(__dirname, 'dist')
