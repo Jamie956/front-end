aeed29c14e2cd281cf393e6cebeffacf6b4a5d31
jamie <752492509@qq.com>
2018-03-28 11:16:48 +0800

    init browser-router

---
+++ react/browser-router/webpack.config.js
@@ -0,0 +1,30 @@
+var path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: "./src/main.js",
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['react', 'es2015']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html'
+    })
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js"
+  }
+};
