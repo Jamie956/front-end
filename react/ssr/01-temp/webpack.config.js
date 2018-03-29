afab5b5da94b452138500fdeabc97356acd21aa7
jamie <752492509@qq.com>
2018-03-29 14:41:10 +0800

    init ssr

---
+++ react/ssr/01-temp/webpack.config.js
@@ -0,0 +1,61 @@
+var path = require('path')
+var webpack = require('webpack')
+var nodeExternals = require('webpack-node-externals')
+
+var browserConfig = {
+  entry: './src/browser/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          babelrc: false,
+          presets: ['env', 'react']
+        }
+      }
+    }]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      __isBrowser__: "true"
+    })
+  ]
+}
+
+var serverConfig = {
+  entry: './src/server/index.js',
+  target: 'node',
+  externals: [nodeExternals()],
+  output: {
+    path: __dirname,
+    filename: 'server.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          babelrc: false,
+          presets: ['env', 'react']
+        }
+      }
+    }]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      __isBrowser__: "false"
+    })
+  ]
+}
+
+module.exports = [browserConfig, serverConfig]
