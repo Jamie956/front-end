e70f8d3be5e0ae604e74459e021a72357bd449ea
jamie <752492509@qq.com>
2018-04-02 00:55:16 +0800

    update loading status

--- cuke/src/components/forms/LoginForm.js
+++ cuke/src/components/forms/LoginForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import FormField, { Form, Button } from "semantic-ui-react";
+import FormField, { Form, Button, Message } from "semantic-ui-react";
 import Validator from "validator";
 import InlineError from "../messages/InlineError";
 import PropTypes from 'prop-types';
@@ -18,7 +18,12 @@ class LoginForm extends Component {
         const errors = this.validate(this.state.data);
         this.setState({ errors });
         if (Object.keys(errors).length === 0) {
-            this.props.submit(this.state.data);
+            this.setState({ loading: true });
+            this.props
+                .submit(this.state.data)
+                .catch(err =>
+                    this.setState({ errors: err.response.data.errors, loading: false })
+                );
         }
     }
 
@@ -30,17 +35,22 @@ class LoginForm extends Component {
     }
 
     onChange = e => {
-        console.log(e.target)
         this.setState({
             data: { ...this.state.data, [e.target.name]: e.target.value }
         })
     }
 
     render() {
-        const { data, errors } = this.state;
+        const { data, errors, loading } = this.state;
 
         return (
-            <Form onSubmit={this.onSubmit}>
+            <Form onSubmit={this.onSubmit} loading={loading}>
+                {errors.global && (
+                    <Message negative>
+                        <Message.Header>Something went wrong</Message.Header>
+                        <p>{errors.global}</p>
+                    </Message>
+                )}
                 <Form.Field error={!!errors.email}>
                     <label htmlFor="email">Email</label>
                     <input
