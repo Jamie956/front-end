c69e28cef0e3754fbe062c259f1c1d4c685134d0
jamie <752492509@qq.com>
2018-04-02 14:14:01 +0800

    mongoose

--- cuke-api/src/index.js
+++ cuke-api/src/index.js
@@ -1,14 +1,25 @@
 import express from 'express';
 import path from 'path';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import Promise from "bluebird";
+import auth from './routes/auth';
 
 const app = express();
+app.use(bodyParser.json());
+mongoose.Promise = Promise;
+mongoose.connect('mongodb://localhost/mydb', { useMongoClient: true });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log("Connection Successful!")
+});
 
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.post('/api/auth', (req, res) => {
- res.status(400).json({ errors: { global: 'Invalid credentials' } });
-});
+app.use('/api/auth', auth);
 
 app.listen(3000, () => console.log('listen on port: 3000'));
