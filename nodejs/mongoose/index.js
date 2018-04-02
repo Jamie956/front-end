57e40356aabb1be7d8de67ea47a91f2e8c95cec5
jamie <752492509@qq.com>
2018-04-02 12:31:55 +0800

    mongoose insert

--- nodejs/mongoose/index.js
+++ nodejs/mongoose/index.js
@@ -8,24 +8,38 @@ db.on('error', console.error.bind(console, 'connection error:'));
 
 db.once('open', function () {
 	console.log("Connection Successful!");
-	// define Schema
-	var BookSchema = mongoose.Schema({
-		name: String,
-		price: Number,
-		quantity: Number
-	});
-
-	// compile schema to model
-	var Book = mongoose.model('Book', BookSchema, 'bookstore');
-
-	// a document instance
-	var book1 = new Book({ name: 'Introduction to Mongoose', price: 10, quantity: 25 });
-
-	// save model to database
-	book1.save(function (err, book) {
-		if (err) return console.error(err);
-		console.log(book.name + " saved to bookstore collection.");
-	});
+	savebook();
+	inserbook();
 });
 
 
+var BookSchema = mongoose.Schema({
+	name: String,
+	price: Number,
+	quantity: Number
+});
+var Book = mongoose.model('Book', BookSchema, 'bookstore');
+
+
+function savebook() {
+	var book1 = new Book({ name: 'Introduction to Mongoose', price: 10, quantity: 25 });
+	book1.save(function (err, book) {
+		if (err)
+			return console.error(err);
+		console.log(book.name + " saved to bookstore collection.");
+	});
+}
+
+function inserbook() {
+	var books = [{ name: 'Mongoose Tutorial', price: 10, quantity: 25 },
+	{ name: 'NodeJS tutorial', price: 15, quantity: 5 },
+	{ name: 'MongoDB Tutorial', price: 20, quantity: 2 }];
+
+	Book.collection.insert(books, function (err, docs) {
+		if (err) {
+			return console.error(err);
+		} else {
+			console.log("Multiple documents inserted to Collection");
+		}
+	});
+}
