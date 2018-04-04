00e2f0d0c3f16e93b96ec8348be7541c76bc7fab
jamie <752492509@qq.com>
2018-04-04 10:41:36 +0800

    init react table tmp

---
+++ react/react-table/00-tmp/src/index.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { render } from "react-dom";
+import ReactTable from "react-table";
+import "react-table/react-table.css";
+
+class App extends React.Component {
+  render() {
+    const data = [{
+      name: 'Diko',
+      age: 12,
+      friend: {
+        name: 'Juju',
+        age: 18,
+      }
+    }, {
+      name: 'Juju',
+      age: 18,
+      friend: {
+        name: 'Pina',
+        age: 25,
+      }
+    }]
+
+    const columns = [{
+      Header: 'Name',
+      accessor: 'name' // String-based value accessors!
+    }, {
+      Header: 'Age',
+      accessor: 'age',
+      Cell: props => <span className='number'>{props.value}</span> // Custom cell components!
+    }, {
+      id: 'friendName', // Required because our accessor is not a string
+      Header: 'Friend Name',
+      accessor: d => d.friend.name // Custom value accessors!
+    }, {
+      Header: props => <span>Friend Age</span>, // Custom header components!
+      accessor: 'friend.age'
+    }]
+
+    const divStyle = {
+      color: 'blue',
+      height: '8%',
+      width: 600
+    };
+
+    return (
+      <div style={divStyle} className="container">
+        <ReactTable
+          data={data}
+          columns={columns}
+        />
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById("root"));
