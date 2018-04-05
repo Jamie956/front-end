a6cc768fb2a942c6c22073b02db10a2753828d1b
jamie <752492509@qq.com>
2018-04-05 22:50:39 +0800

    update

--- webpack/09-image-loader/webpack.config.js
+++ webpack/09-image-loader/webpack.config.js
@@ -1,20 +1,22 @@
 const path = require('path');
 
 module.exports = {
-	entry: './src/main.js',
-	module: {
-		rules: [{
-			test: /\.(png|jpg)$/,
-			use: [{
-				loader: 'url-loader',
-				options: {
-					limit: 8192
-				}
-			}]
-		}]
-	},
+	entry: './src/index.js',
 	output: {
 		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'dist')
-	}
+		path: path.resolve(__dirname, 'build')
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(png|jpg)$/,
+				use: [{
+					loader: 'url-loader',
+					options: {
+						limit: 8192
+					}
+				}]
+			}
+		]
+	},
 };
