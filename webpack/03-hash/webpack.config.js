55743f6d9e35954ee17198512444b662fbc2be9b
jamie <752492509@qq.com>
2018-04-05 22:01:32 +0800

    update

--- webpack/03-hash/webpack.config.js
+++ webpack/03-hash/webpack.config.js
@@ -2,11 +2,11 @@ const path = require('path');
 
 module.exports = {
 	entry: {
-		app: './src/app',
-		vendors: './src/vendors'
+		foo: './src/foo',
+		bar: './src/bar'
 	},
 	output: {
-		filename: '[name]-[hash].js',
-		path: path.resolve(__dirname, 'dist')
+		filename: '[name].[hash].js',
+		path: path.resolve(__dirname, 'build')
 	}
 };
