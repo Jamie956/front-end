614c7d28849b33b4f739f517366665bfee415d7f
jamie <752492509@qq.com>
2018-04-05 22:39:27 +0800

    update

--- webpack/07-babel-loader/webpack.config.js
+++ webpack/07-babel-loader/webpack.config.js
@@ -1,22 +1,23 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/main.js',
-  module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          babelrc: false,
-          presets: ['es2015']
-        }
-      }
-    }]
-  },
+  entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, './dist')
-  }
+    path: path.resolve(__dirname, 'build')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015']
+          }
+        }
+      }
+    ]
+  },
 };
