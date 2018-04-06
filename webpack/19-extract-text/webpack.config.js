e43512d467829e4462fae486b2e235ef0c40c111
jamie <752492509@qq.com>
2018-04-06 10:23:44 +0800

    update

--- webpack/19-extract-text/webpack.config.js
+++ webpack/19-extract-text/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: './src/main.js',
+  entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build')
