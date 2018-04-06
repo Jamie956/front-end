00e75745c52622981cff0ff6dad82ff0daab3d0f
jamie <752492509@qq.com>
2018-04-06 10:16:08 +0800

    update

--- webpack/17-dev-server/webpack.config.js
+++ webpack/17-dev-server/webpack.config.js
@@ -2,18 +2,18 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	entry: './src/main.js',
+	entry: './src/index.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'build')
+	},
 	plugins: [
 		new HtmlWebpackPlugin({
 			template: "./src/index.html"
 		})
 	],
 	devServer: {
-		contentBase: './dist',
+		contentBase: './build',
 		port: 3000
 	},
-	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'dist')
-	}
 };
