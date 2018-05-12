251a86fd90623f3cc7eb516b2afd7f60fe3785b5
jamie956 <752492509@qq.com>
2018-05-12 10:13:39 +0800

    module exports

--- 04-createModule/index.js
+++ 04-createModule/index.js
@@ -1,5 +1,14 @@
-var currency = require('./currency');
-console.log('50 Canadian dollars equals this amount of US dollars:');
+//==============test currency1
+
+// var currency = require('./currency');
+// console.log('50 Canadian dollars equals this amount of US dollars:');
+// console.log(currency.canadianToUS(50));
+// console.log('30 US dollars equals this amount of Canadian dollars:');
+// console.log(currency.USToCanadian(30));
+
+//==============test currency2
+
+var Currency = require('./currency2')
+, canadianDollar = 0.91;
+currency = new Currency(canadianDollar);
 console.log(currency.canadianToUS(50));
-console.log('30 US dollars equals this amount of Canadian dollars:');
-console.log(currency.USToCanadian(30));
