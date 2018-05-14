d42c081113e34ff82a94a53d5f44ab7dd3add9dd
jamie <jamie@justek.us>
2018-05-14 14:25:59 +0800

    update

--- 10-staticFileServer/index.js
+++ 10-staticFileServer/index.js
@@ -18,9 +18,43 @@
 // server.listen(3000);
 
 //=======================
+// var server = http.createServer(function (req, res) {
+//     var url = parse(req.url);
+//     var path = join(root, url.pathname);
+//     var stream = fs.createReadStream(path);
+//     stream.pipe(res);
+// });
+
+//=======================
+//error
+// stream.pipe(res);
+// stream.on('error', function (err) {
+//     res.statusCode = 500;
+//     res.end('Internal Server Error');
+// });
+
+//=======================
+// file_server.js
 var server = http.createServer(function (req, res) {
     var url = parse(req.url);
     var path = join(root, url.pathname);
-    var stream = fs.createReadStream(path);
-    stream.pipe(res);
+    fs.stat(path, function (err, stat) {
+        if (err) {
+            if ('ENOENT' == err.code) {
+                res.statusCode = 404;
+                res.end('Not Found');
+            } else {
+                res.statusCode = 500;
+                res.end('Internal Server Error');
+            }
+        } else {
+            res.setHeader('Content-Length', stat.size);
+            var stream = fs.createReadStream(path);
+            stream.pipe(res);
+            stream.on('error', function (err) {
+                res.statusCode = 500;
+                res.end('Internal Server Error');
+            });
+        }
+    });
 });
