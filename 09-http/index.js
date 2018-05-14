45f961e9515e8955ddc65f7f9236b10832d755c2
jamie <jamie@justek.us>
2018-05-14 11:25:07 +0800

    update http

--- 09-http/index.js
+++ 09-http/index.js
@@ -76,19 +76,19 @@
 // res.end(body);
 
 //=======================
-switch () {
-    case 'DELETE':
-        var path = url.parse(req.url).pathname;
-        var i = parseInt(path.slice(1), 10);
-        if (isNaN(i)) {
-            res.statusCode = 400;
-            res.end('Invalid item id');
-        } else if (!items[i]) {
-            res.statusCode = 404;
-            res.end('Item not found');
-        } else {
-            items.splice(i, 1);
-            res.end('OK\n');
-        }
-        break;
-}
+// switch () {
+//     case 'DELETE':
+//         var path = url.parse(req.url).pathname;
+//         var i = parseInt(path.slice(1), 10);
+//         if (isNaN(i)) {
+//             res.statusCode = 400;
+//             res.end('Invalid item id');
+//         } else if (!items[i]) {
+//             res.statusCode = 404;
+//             res.end('Item not found');
+//         } else {
+//             items.splice(i, 1);
+//             res.end('OK\n');
+//         }
+//         break;
+// }
