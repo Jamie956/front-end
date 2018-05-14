040940f3b4e78eb2c2872a4ac5ded5bcfb11a280
jamie <jamie@justek.us>
2018-05-14 11:32:28 +0800

    update fs

--- 01-fileSystem/index.js
+++ 01-fileSystem/index.js
@@ -1,12 +1,14 @@
-var fs = require('fs');
-
+//======================
+//read file as utf8
+// var fs = require('fs');
 // fs.readFile('./resource.json', 'utf8', function (err, data) {
 //     if (err) throw err;
 //     console.log(data);
 // })
 
-//================
-
+//======================
+//read file as stream
+// var fs = require('fs');
 // var stream = fs.createReadStream('./resource.json')
 // stream.on('data', function (chunk) {
 //     console.log(chunk)
@@ -15,12 +17,11 @@ var fs = require('fs');
 //     console.log('finished')
 // })
 
-//================
-
+//======================
+//read file as stream and pipe to http respon
 var http = require('http');
 var fs = require('fs');
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'image/jpg' });
     fs.createReadStream('./image.jpg').pipe(res);
 }).listen(3000);
-console.log('Server running at http://localhost:3000/');
