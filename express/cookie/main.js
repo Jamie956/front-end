1da24ba77ba16caeea83cce3fd7223f11be5b3fc
jamie <jamie@justek.us>
2018-05-18 17:51:17 +0800

    set cookies

--- express/cookie/main.js
+++ express/cookie/main.js
@@ -4,6 +4,13 @@ var cookieParser = require('cookie-parser');
 var app = express();
 app.use(cookieParser());
 
-app.get('/', (req, res) => res.send(req.cookies));
+app.get('/get', (req, res) => {
+    res.send(req.cookies)
+});
+
+app.get('/set', (req, res) => {
+    res.cookie('name', 'tom', { expires: new Date(Date.now() + 900000), httpOnly: true });
+    res.end('ok');
+});
 
 app.listen(3000, () => console.log('listen on port: 3000'));
