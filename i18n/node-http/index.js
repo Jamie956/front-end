bc102fe26f12fd9a03e21623dd9d05f6ade6bb3e
jamie <jamie@justek.us>
2018-05-18 14:54:23 +0800

    init i18n

---
+++ i18n/node-http/index.js
@@ -0,0 +1,16 @@
+var i18n = require('i18n');
+var http = require('http');
+var url = require('url');
+var app;
+
+i18n.configure({
+  locales: ['en', 'zh'],
+  directory: __dirname + '/locales'
+});
+
+app = http.createServer(function (req, res) {
+  i18n.init(req, res);
+  res.end(res.__('Hello World'));
+});
+
+app.listen(3000, '127.0.0.1');
