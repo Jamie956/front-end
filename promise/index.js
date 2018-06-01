002e75daeb813e57d3b1ae0ddc6b19398c454560
jamie956 <752492509@qq.com>
2018-06-01 21:35:19 +0800

    promise

---
+++ promise/index.js
@@ -0,0 +1,25 @@
+// doSomething()
+// .then(result => doSomethingElse(result))
+// .then(newResult => doThirdThing(newResult))
+// .then(finalResult => {
+//   console.log(`Got the final result: ${finalResult}`);
+// })
+// .catch(failureCallback);
+
+//
+new Promise((resolve, reject) => {
+  console.log("Initial");
+
+  resolve();
+})
+  .then(() => {
+    throw new Error("Something failed");
+
+    console.log("Do this");
+  })
+  .catch(() => {
+    console.log("Do that");
+  })
+  .then(() => {
+    console.log("Do this whatever happened before");
+  });
