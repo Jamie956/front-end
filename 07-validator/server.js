2161274ad776feeb0cf8b8b241a94af8dc891641
jamie956 <752492509@qq.com>
2018-06-02 09:50:51 +0800

    update valid

--- 07-validator/server.js
+++ 07-validator/server.js
@@ -1,21 +1,30 @@
-var validator = require('validator');
+var vd = require("validator");
 
-var result = validator.isEmail('foo@bar.com'); //=> true
+//api
+// console.log(vd.isEmail('foo@bar.com'))
+// console.log(vd.isEmpty(''))
+// console.log(vd.isJSON(JSON.stringify({name: 'tom'})))
+// console.log(vd.isNumeric('123'))
+// console.log(vd.isLowercase('a'))
+// console.log(vd.isUppercase('a'))
+// console.log(vd.blacklist('abcdefga','a'))
+// console.log(vd.escape('< - > - & - /'))
 
-console.log(result)
+//form
+var data = {
+  name: "tom",
+  age: 18,
+  email: "tom@email.com",
+  password: "abc123"
+};
 
-// isEmpty(str)
+function myVd(data) {
+  if (vd.isEmpty(data["name"])) return "name should not be empty";
+  if (!vd.isNumeric("123")) return "age should be num";
+  if (!vd.isEmail(data["email"])) return "not email";
+  if (!vd.isLength(data["password"], { min: 2, max: 6 })) return "pwd should 2-6";
 
-// isJSON(str)
-isNumeric(str)
+  return true;
+}
 
-isLowercase(str)
-isUppercase(str)
-
-
-var blacklist = validator.blacklist('abcdefga','a');  
-console.log('blacklist :', blacklist); 
-
-
-var escape = validator.escape('< - > - & - /');  
-console.log('escape :', escape);  
+console.log(myVd(data));
