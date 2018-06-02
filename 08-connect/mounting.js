8b63dc7a3b71610d0e2bcc55132e4a05087c100a
jamie956 <752492509@qq.com>
2018-06-03 01:14:42 +0800

    update middle

--- 08-connect/mounting.js
+++ 08-connect/mounting.js
@@ -10,8 +10,23 @@ const hello = (req, res) => {
   res.end("hello world");
 };
 
+const admin = (req, res, next) => {
+  console.log("admin");
+  next();
+};
+const restrict = (req, res, next) => {
+  console.log("restrict");
+  let isAuth = false;
+  if (isAuth) {
+    next();
+  } else {
+    next(new Error("failure to auth"));
+  }
+};
 const app = connect()
   .use(logger)
+  .use("/admin", restrict)
+  .use("/admin", admin)
   .use(hello);
 
 app.listen(3000, console.log("listen on port 3000"));
