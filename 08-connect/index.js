5d04bbe2ac166adbf71ce7a4ad2c2cc58b61f1df
jamie956 <752492509@qq.com>
2018-06-03 00:33:47 +0800

    update connect

--- 08-connect/index.js
+++ 08-connect/index.js
@@ -1,3 +1,10 @@
-const connect = require('connect')
-const app = connect()
-app.listen(3000)
+const connect = require("connect");
+const app = connect();
+app.listen(3000, console.log("listen on port 3000"));
+
+const logger = (req, res, next) => {
+  console.log("%s %s", req.method, req.url);
+  next();
+};
+
+app.use(logger);
