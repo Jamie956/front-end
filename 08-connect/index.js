d3822af1ba647d462b4708b836cd948dc68b10c6
jamie956 <752492509@qq.com>
2018-06-03 00:39:26 +0800

    update middle

--- 08-connect/index.js
+++ 08-connect/index.js
@@ -1,10 +1,16 @@
 const connect = require("connect");
 const app = connect();
-app.listen(3000, console.log("listen on port 3000"));
 
 const logger = (req, res, next) => {
   console.log("%s %s", req.method, req.url);
   next();
 };
-
 app.use(logger);
+
+const hello = (req, res) => {
+  res.setHeader("Content-Type", "text/plain");
+  res.end("hello world");
+};
+app.use(hello);
+
+app.listen(3000, console.log("listen on port 3000"));
