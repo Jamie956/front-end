e55abf01c2e975a22c59885b32d9a7a4bad2d326
jamie956 <752492509@qq.com>
2018-06-03 16:51:27 +0800

    update middle

--- 08-middleware/01/index.js
+++ 08-middleware/01/index.js
@@ -5,6 +5,14 @@ const routes = {
   GET: {
     "/users": (req, res) => {
       res.end("tom, mik");
+    },
+    "/user/:id": (req, res, id) => {
+      res.end("get user id " + id);
+    }
+  },
+  POST: {
+    "/user": (req, res) => {
+      res.end("post user");
     }
   }
 };
--- 08-middleware/01/router.js
+++ 08-middleware/01/router.js
@@ -3,8 +3,27 @@ const parse = url.parse;
 
 const router = obj => {
   return (req, res, next) => {
-    console.log(req.method);
-    console.log(obj);
+    if (!obj[req.method]) {
+      next();
+      return;
+    }
+    let url = parse(req.url); //req url
+    let routes = obj[req.method];
+    let paths = Object.keys(routes); //config keys
+
+    for (let i = 0; i < paths.length; i++) {
+      let path = paths[i];
+      const fn = routes[path];
+      path = path.replace(/\//g, "\\/").replace(/:(\w+)/g, "([^\\/]+)");
+      const re = new RegExp("^" + path + "$");
+      let captures = url.pathname.match(re);
+      if (captures) {
+        const args = [req, res].concat(captures.slice(1));
+        fn.apply(null, args);
+        return;
+      }
+    }
+    next();
   };
 };
 
