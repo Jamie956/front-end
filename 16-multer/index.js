561049eccbcdd05ade83b082fe5dd7523d9bb8ce
jamie <752492509@qq.com>
2018-06-04 12:29:44 +0800

    update upload

--- 16-multer/index.js
+++ 16-multer/index.js
@@ -9,7 +9,7 @@ const storage = multer.diskStorage({
   //   destination: function(req, file, cb) {
   //     cb(null, path.resolve("images"));
   //   },
-  destination: path.resolve("images"),
+  destination: path.resolve("images"), //auto create dir
   filename: function(req, file, cb) {
     cb(
       null,
@@ -20,6 +20,11 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 app.post("/upload", upload.single("logo"), (req, res, next) => {
-  res.end("ok");
+  console.log(req.file);
+  if (req.file) {
+    res.end("ok");
+  } else {
+    res.end("fail");
+  }
 });
 app.listen(3000, console.log("listen on port 3000"));
