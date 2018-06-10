e15463c31fe4875e0174a2f5a4e44b5c69307c10
jamie956 <752492509@qq.com>
2018-06-10 23:38:54 +0800

    update promise

---
+++ promise/01.js
@@ -0,0 +1,10 @@
+var promise = new Promise(function(resolve) {
+  resolve(42);
+});
+promise
+  .then(function(value) {
+    console.log(value);
+  })
+  .catch(function(error) {
+    console.error(error);
+  });
