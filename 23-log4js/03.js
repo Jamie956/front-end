fb7873200c308545ad99e979c8703236cc4f12f7
jamie956 <752492509@qq.com>
2018-06-16 21:04:57 +0800

    update log4js

--- 23-log4js/03.js
+++ 23-log4js/03.js
@@ -1,14 +1,16 @@
-const express = require('express')
+const express = require("express");
 var app = express();
 const log4js = require("./log-config");
-const logger = log4js.getLogger(); //根据需要获取logger
+const logger = log4js.getLogger();
 const errlogger = log4js.getLogger("err");
 const othlogger = log4js.getLogger("oth");
 
-//结合express使用，记录请求日志
-// app = express();
-log4js.useLogger(app, logger); //这样会自动记录每次请求信息，放在其他use上面
-//手动记录，可以代替console.log
-logger.info("test info 1");
-errlogger.err("test error 1");
-othlogger.info("test info 2");
+log4js.useLogger(app, logger);
+
+app.get("/", (req, res) => {
+  logger.info("test info 1");
+//   errlogger.err("test error 1");
+  othlogger.info("test info 2");
+  res.send("hi");
+});
+app.listen(3000, console.log("listen on port 3000"));
