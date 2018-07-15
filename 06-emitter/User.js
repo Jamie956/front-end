8cff87f7d4a424a5272fda98ae5884272d447e5a
jamie956 <752492509@qq.com>
2018-07-15 08:15:40 +0800

    update

--- 06-emitter/User.js
+++ 06-emitter/User.js
@@ -4,17 +4,17 @@ class User extends EventEmitter {
   constructor() {
     super();
     //初始化
-    this.users = [{ name: "tom" }];
+    this.users = [{ name: "tom", email: "tom@email.com" }];
   }
-
-  add(name) {
-    this.users.push({ name });
-    this.emit("change");
+  add(u) {
+    this.users.push(u); //添加数据
+    this.emit("change"); //发出通知，监听change事件的emiiter被触发
   }
-
+  //打印数据
   print() {
     console.log(this.users);
   }
 }
 
+//导出函数
 module.exports = User;
--- 06-emitter/index.js
+++ 06-emitter/index.js
@@ -2,12 +2,13 @@ const User = require("./User");
 
 const user = new User();
 
+//emitter监听change事件
 user.on("change", () => {
   console.log("change!!");
 });
 
 user.print();
 
-user.add("newbee");
+user.add({ name: "jerry",email:"jerry@email.com" });
 
 user.print();
