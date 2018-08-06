82e72ecbdecec2f48ad7ba0878f03ed1819229f8
jamie <jamie@justek.us>
2018-08-06 14:22:44 +0800

    update

--- x-proxy/api.js
+++ x-proxy/api.js
@@ -2,7 +2,7 @@ var http = require("http");
 
 var server = http.createServer(function(req, res) {
   console.log("req");
-  res.end("ax");
+  res.end("api data");
 });
 
 server.listen(8082, console.log("listening on port 8082"));
