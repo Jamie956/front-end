68c284f5648f8f8ee7e609f413af6f6aa3ae7d3d
jamie <jamie@justek.us>
2018-08-06 17:03:39 +0800

    update

--- x-proxy/01.js
+++ x-proxy/01.js
@@ -1,7 +1,7 @@
 var http = require("http");
 var httpProxy = require("http-proxy");
 
-//代理9000端口，当访问8000时，代理访问9000端口
+//8000端口的服务，当访问8000时，代理访问9000端口
 httpProxy
   .createProxyServer({ target: "http://localhost:9000" })
   .listen(8000, console.log("Proxy server listening on port 8000"));
