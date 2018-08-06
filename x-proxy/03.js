4efccfd33a93363b19d2a49d93d96b3973a87620
jamie <jamie@justek.us>
2018-08-06 16:42:18 +0800

    update

--- x-proxy/03.js
+++ x-proxy/03.js
@@ -4,8 +4,23 @@ var http = require("http"),
 //创建代理服务
 var proxy = httpProxy.createProxyServer({});
 
-proxy.on('proxyReq', function(proxyReq, req, res, options) {
-  proxyReq.setHeader('X-Special-Proxy-Header', 'foobar');
+// proxyReq: This event is emitted before the data is sent. It gives you a chance to alter the proxyReq request object. Applies to "web" connections
+proxy.on("proxyReq", function(proxyReq, req, res, options) {
+  proxyReq.setHeader("X-Special-Proxy-Header", "foobar");
+});
+
+proxy.on("proxyRes", function(proxyRes, req, res) {
+  console.log(
+    "RAW Response from the target",
+    JSON.stringify(proxyRes.headers, true, 2)
+  );
+});
+
+proxy.on("error", function(err, req, res) {
+  res.writeHead(500, {
+    "Content-Type": "text/plain"
+  });
+  res.end("Something went wrong. And we are reporting a custom error message.");
 });
 
 var server = http.createServer(function(req, res) {
