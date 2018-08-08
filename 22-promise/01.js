496e71a558d2004b180d5b19e82d1673fd674719
jamie <jamie@justek.us>
2018-08-08 18:12:47 +0800

    update

--- 22-promise/01.js
+++ 22-promise/01.js
@@ -11,7 +11,7 @@ function test01() {
 //创建pomise对象，返回resolve
 function test02() {
   var promise = new Promise(reject => {
-    reject(new Error("...BOOM..."));
+    reject(new Error("Something Wrong"));
   });
   promise
     .then(res => {
@@ -28,7 +28,7 @@ function test03() {
     if (true) {
       resolve(42);
     } else {
-      reject(new Error("...BOOM..."));
+      reject(new Error("Something Wrong"));
     }
   });
   promise
@@ -40,4 +40,73 @@ function test03() {
     });
 }
 
-test03();
+//调用异步函数，返回promise对象
+function test04() {
+  function asyncFun() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve("Got it");
+      }, 2000);
+    });
+  }
+  asyncFun()
+    .then(data => {
+      console.log(data);
+    })
+    .catch(e => {
+      console.log(e);
+    });
+  console.log("Go on");
+}
+
+//promise语法糖，返回resolve
+function test05() {
+  var promise = Promise.resolve(42);
+  promise.then(res => {
+    console.log(res);
+  });
+}
+
+//promise语法糖，返回reject
+function test06() {
+  var promise = Promise.reject(new Error("Something Wrong"));
+  promise.catch(e => {
+    console.log(e);
+  });
+}
+
+//调用链错误处理
+function test07() {
+  var promise = Promise.resolve();
+
+  promise
+    .then(() => console.log("task a"))
+    .then(() => {
+      console.log("task b");
+      // throw new Error("task b meet with error");
+    })
+    .then(() => console.log("finish"))
+    .catch(e => {
+      console.log(e);
+    });
+}
+
+//调用链传递返回值
+function test08() {
+  var promise = Promise.resolve(1);
+  promise
+    .then(val => {
+      return val * 2;
+    })
+    .then(val => {
+      return val * 2;
+    })
+    .then(val => {
+      console.log(val);
+    })
+    .catch(e => {
+      console.log(e);
+    });
+}
+
+test07();
--- 22-promise/05.js
+++
@@ -1,4 +0,0 @@
-//thenable object convert to promise object
-Promise.resolve($.ajax("http://azu.github.io/promises-book/json/people.json")).then(val => {
-  console.log(val);
-});
