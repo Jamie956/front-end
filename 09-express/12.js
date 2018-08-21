fdeb5f491f77a3df4e2a7cd825d7df70c82df240
jamie <jamie@justek.us>
2018-08-21 11:48:15 +0800

    up

--- 09-express/12.js
+++
@@ -1,9 +0,0 @@
-//send file
-var express = require("express");
-var app = express();
-var path = require("path");
-
-var filePath = path.resolve("public/hello.txt");
-app.get("/", (req, res) => res.sendFile(filePath));
-
-app.listen(3000, console.log("LISTEN ON PORT: 3000"));
--- 09-express/README.md
+++ 09-express/README.md
@@ -1,4 +1,7 @@
-===create skeleton===
+```
+生成express骨架
 npm i -g express-generator
+
 express --help
 express -e photo
+```
