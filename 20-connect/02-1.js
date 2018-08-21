c33e8d2f3f1867d630d78ebb0aa7928e3297eba3
jamie <jamie@justek.us>
2018-08-21 10:46:42 +0800

    up

--- 20-connect/02-1.js
+++
@@ -1,7 +0,0 @@
-//static file serving
-var connect = require("connect");
-var app = connect();
-var static = require("serve-static");
-
-//http://localhost:3000/foo.js
-app.use(static("public")).listen(3000, console.log("LISTEN ON PORT 3000"));
--- 20-connect/02-2.js
+++
@@ -1,9 +0,0 @@
-//using static with mounting
-var connect = require("connect");
-var app = connect();
-var static = require("serve-static");
-
-//http://localhost:3000/app/files/foo.js
-app
-  .use("/app/files", static("public"))
-  .listen(3000, console.log("LISTEN ON PORT 3000"));
