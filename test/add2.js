3d9d3e5e8b1165d25e1b58a4a0c71d16fd15fa1a
Jamie <752492509@qq.com>
2019-01-11 15:26:20 +0800

    up

--- test/add2.js
+++
@@ -1,7 +0,0 @@
-function add(a, b) {
-  if (typeof a === "number" && typeof b === "number") {
-    return a + b;
-  } else {
-    return undefined;
-  }
-}
--- test/server.js
+++
@@ -1,8 +0,0 @@
-var http = require("http");
-var server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    "Content-Type": "text/plain"
-  });
-  res.end("Hello Fundebug");
-});
-server.listen(8000);
