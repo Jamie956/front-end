f579406c5c1e3230fff8b5b03b7e927fe7a5c311
Jamie <752492509@qq.com>
2019-05-30 11:06:47 +0800

    up

--- mem/i1.js
+++ mem/i1.js
@@ -1,21 +1,17 @@
-var showMem = function() {
-  var mem = process.memoryUsage();
-  console.log(mem.heapUsed/1000000000);
-};
-
-var useMem = function() {
+function useMem() {
   var size = 20 * 1024 * 1024;
   var arr = new Array(size);
   for (let i = 0; i < size; i++) {
     arr[i] = 0;
   }
   return arr;
-};
+}
 
 var total = [];
 
 for (let j = 0; j < 15; j++) {
-  showMem();
+  var mem = process.memoryUsage();
+  console.log(mem.heapUsed / (1024 * 1024 * 1024));
+
   total.push(useMem());
 }
-showMem();
--- mem/i2.js
+++ mem/i2.js
@@ -1,22 +1,18 @@
-var showMem = function() {
-  var mem = process.memoryUsage();
-  console.log(mem.heapUsed/1000000000);
-};
-
-//堆外内存使用测试
-var useMem = function() {
+//堆外内存
+function useMem() {
   var size = 200 * 1024 * 1024;
   var buffer = new Buffer(size);
   for (var i = 0; i < size; i++) {
     buffer[i] = 0;
   }
   return buffer;
-};
+}
 
 var total = [];
 
 for (let j = 0; j < 15; j++) {
-  showMem();
+  var mem = process.memoryUsage();
+  console.log(mem.heapUsed / (1024 * 1024 * 1024));
+
   total.push(useMem());
 }
-showMem();
